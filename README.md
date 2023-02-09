b303e588a6498767d31d8b099248374bb53c1ca1
wk-j <somnuk.wk@gmail.com>
2023-02-09 23:44:16 +0700

    Patch

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Partial update model using `JsonPatchDocument`
+- Partial update model using `JsonPatchDocument`
+- https://learn.microsoft.com/en-us/aspnet/core/web-api/jsonpatch?view=aspnetcore-7.0
--- src/MyWeb/Program.cs
+++ src/MyWeb/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
+builder.Services.AddControllers(options => {
+    options.InputFormatters.Insert(0, MyJPIF.GetJsonPatchInputFormatter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,3 +28,20 @@ if (app.Environment.IsDevelopment()) {
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+public static class MyJPIF {
+    public static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() {
+        var builder = new ServiceCollection()
+            .AddLogging()
+            .AddMvc()
+            .AddNewtonsoftJson()
+            .Services.BuildServiceProvider();
+
+        return builder
+            .GetRequiredService<IOptions<MvcOptions>>()
+            .Value
+            .InputFormatters
+            .OfType<NewtonsoftJsonPatchInputFormatter>()
+            .First();
+    }
+}
